c65edd7a6916ae2820aeff2e684d8364ceec9bfc
越锋 陈 <3510329812@qq.com>
2020-04-09 15:19:50 +0800

    添加项目文件。

---
+++ Log4net.Logger/LogLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace Log4net.Logger
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+}
---
+++ Log4netTest/Log4netForm.cs
@@ -0,0 +1,30 @@
+﻿using Log4net.Logger;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log4netTest
+{
+	public partial class Log4netForm : Form
+	{
+		public Log4netForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnExportMsg_Click(object sender, EventArgs e)
+		{		
+			LogUtil.Instance.Debug("Debug Info");		
+			LogUtil.Instance.Warn("Warn Info");
+			LogUtil.Instance.Error("Eorror Info");
+			LogUtil.Instance.Fatal("Fatal Info");
+			LogUtil.Instance.Info("Info Info");
+		}
+	}
+}
